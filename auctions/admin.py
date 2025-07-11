bd836fc4a0c6d706dcd9eedc3d9748ad5c2366f2
duanniten <duanniten>
2025-07-11 05:16:22 -0300

    register models in admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bid, Listing, User, Category
+
+admin.site.register(Bid)
+admin.site.register(Listing)
+admin.site.register(User)
+admin.site.register(Category)
