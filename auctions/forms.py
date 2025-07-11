7b45882f60155bdc36003f15308c999124f268a6
Duann Iten <duann_@hotmail.com>
2025-07-11 18:26:59 -0300

    Create Forms in forms.py

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 
-from .models import Listing
+from .models import Listing, Category, Bid
 
 class CreateListingForm(ModelForm):
     class Meta:
@@ -9,5 +9,26 @@ class CreateListingForm(ModelForm):
             "title",
             "description",
             "imageURL",
-            ""
+            "initBid",
+            "category"            
+        ]
+class CreateCategory(ModelForm):
+    class Meta:
+        models = Category
+        fields = [
+            'type'
+        ]
+
+class MakeBid(ModelForm):
+    class Meta:
+        models = Bid
+        fields = [
+            "value"
+        ]
+
+class CloseListing(ModelForm):
+    class Meta:
+        models = Listing
+        fields = [
+            "closed"
         ]
